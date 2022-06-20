6af1cf7b3cc8d61bc1bd8ad0e5a6a43deb4574f8
kappyQuynh <KappyQuynh@gmail.com>
2022-06-20 18:42:12 +0700

    update auth and cors

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-7ki5mzrd.us.auth0.com',            // Auth0 domain
-  clientId: 'WPf9JzfewD0AuJPeoUYmDmb1n4KdmeWf',          // Auth0 client id
+  clientId: 'GCvULgWIycoeem6zMCOPA3B65CrBrdWT',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
